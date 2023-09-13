5fc44b73bc6ea1c9dfaff0e9f3e8b2c8bf69de5b
Paola Andueza <141682568+P40l44nd8eza@users.noreply.github.com>
2023-09-13 12:50:53 -0600

    Update triangulo.js

--- triangulo.js
+++ triangulo.js
@@ -3,6 +3,6 @@ var operacion;
 
 base = prompt ("Ingrese la Base: ");
 altura = prompt ("Ingrese la altura: ");
-operacion= base * altura/2;
+operacion=(base * altura)/2;
 resultado =operacion;
 alert ("El área del triángulo es "+ resultado);
